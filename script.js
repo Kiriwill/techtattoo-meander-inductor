841e8b405a3374a090520f1304a317a3dbd93c4c
Willian Pacheco <willian.pacheco@mobiplus.com.br>
2023-05-10 13:10:08 -0300

    add calculo

--- script.js
+++ script.js
@@ -11,39 +11,62 @@ var posX = 3
 var posY = 30
 
 function draw() {
-  ctx.clearRect(0, 0, c.width, c.height);
+	ctx.clearRect(0, 0, c.width, c.height);
 
-  //Definição geral do grafico
-  var dpi = parseFloat(document.getElementById('dpi').value) / 25.4;
-  var turns = parseFloat(document.getElementById('turns').value);
-  var line_heg = parseFloat(document.getElementById('ant_height').value) * dpi;
-  var line_len = parseFloat(document.getElementById('cond_thickness').value) * dpi;
-  var gap_btw_lines = parseFloat(document.getElementById('cond_spacing').value) * dpi;
-  var line_dist = (line_len + gap_btw_lines)
+	//Valores brutos
+	var N = parseFloat(document.getElementById('turns').value)
+	var h = parseFloat(document.getElementById('ant_height').value)
+	var gap = parseFloat(document.getElementById('cond_spacing').value)
+	var W = parseFloat(document.getElementById('cond_thickness').value)
+	var d = W + gap
+	var a = W * 2 + gap
 
-  //Image
-  c.height = dpi * parseFloat(document.getElementById('img_height').value) 
-  c.width = dpi * parseFloat(document.getElementById('img_width').value)
-    
-  //Bases
-  var lowerBasePosY = posY + line_heg
-  var highBasePosY = posY + line_len
+	//Definição geral do grafico
+	var dpi = parseFloat(document.getElementById('dpi').value) / 25.4;
+	var turns = N;
+	var line_heig = h * dpi;
+	var line_thick = W * dpi;
+	var gap_btw_lines = gap * dpi;
+	var line_dist = (line_thick + gap_btw_lines)
 
-  //Linha
-  var linePosX = line_n => posX + line_dist * line_n
-  var linePosY = posY + line_len
+	//Image
+	c.height = dpi * parseFloat(document.getElementById('img_height').value) 
+	c.width = dpi * parseFloat(document.getElementById('img_width').value)
 
-  //Formas
-  var line = line_n => ctx.fillRect(linePosX(line_n), linePosY, line_len, line_heg)
-  var higherBase = line_n => ctx.fillRect(linePosX(line_n), highBasePosY, line_dist, line_len)
-  var lowerBase = line_n => ctx.fillRect(linePosX(line_n), lowerBasePosY, line_dist, line_len)
+	//Bases
+	var lowerBasePosY = posY + line_heig
+	var highBasePosY = posY + line_thick
 
+	//Linha
+	var linePosX = line_n => posX + line_dist * line_n
+	var linePosY = posY + line_thick
 
-  for (i = 0; i < turns; i++) {
-    line(i)
-    i % 2 ? higherBase(i) : lowerBase(i)
-  }
+	//Formas
+	var line = line_n => ctx.fillRect(linePosX(line_n), linePosY, line_thick, line_heig)
+	var higherBase = line_n => ctx.fillRect(linePosX(line_n), highBasePosY, line_dist, line_thick)
+	var lowerBase = line_n => ctx.fillRect(linePosX(line_n), lowerBasePosY, line_dist, line_thick)
+
+	console.log("a = ", a)
+	console.log("h = ", h) 
+	console.log("N = ", N)
+	console.log("d = ", d)
+	console.log("W = ", W)
+
+	document.getElementById('inductance').value = inductanceCalc(a, N, h, d, W)
+
+	for (i = 0; i < turns; i++) {
+		line(i)
+		i % 2 ? higherBase(i) : lowerBase(i)
+	}
 
 }
 
+function inductanceCalc(a, N, h, d, W) {
+	return 0.0026 * (a ** 0.0603) * (h ** 0.4429) * (N ** 0.954) * (d ** 0.606) * (W ** -0.173)
+}
+
 draw()
+
+function download() {
+	console.log("TODO")
+}
